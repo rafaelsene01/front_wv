43889a58a4d863daa874db673ca9f8ff8614397f
rafaelsene01 <rafaelssene@gmail.com>
2020-09-09 16:00:40 -0300

    fix:

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
-app = express();
+var express = require("express");
+var path = require("path");
+var serveStatic = require("serve-static");
+
+var app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
-const port = process.env.PORT || 80;
+
+var port = process.env.PORT || 5000;
 app.listen(port);
+console.log("server started " + port);
